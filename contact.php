b95c6cc69caecb661cbe3fed0f7048d8deed55a7
Redice <you@example.com>
2020-05-27 01:22:42 +0200

    yes

--- contact.php
+++ contact.php
@@ -20,9 +20,10 @@
 <body>
 <main>
     <nav class="navbar">
-        <a class="logo" href="index.html"> <img class="x" src="assets/photos/logoconcept.png" width="290"
+        <a class="logo" href="index.html"> <img class="x" src="photos/logoconcept.png" width="290"
                                                 height="60"></a>
         <ul class="nav-links">
+            <li class="responsive"><a href="index.html" style="color: #3C3C3C;">Home</a></li>
             <li class="nav-item hvr-underline-from-left"><a style="    color: #3C3C3C;
 " href="verhuizen.html">Verhuizen</a></li>
             <li class="nav-item hvr-underline-from-left"><a style="    color: #3C3C3C;
