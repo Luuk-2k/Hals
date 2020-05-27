4fe231e451d2ce21d7c92c097204bd39e97286fd
Redice <you@example.com>
2020-05-27 02:31:12 +0200

    yes

--- contact.php
+++ contact.php
@@ -1,21 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-<!DOCTYPE html>
-<html lang="en">
-<link href="contact.css" rel="stylesheet">
-<link href="hover.css" rel="stylesheet">
-<head>
+    <link rel="icon" href="photos/tools-solid.svg" type="image/gif" sizes="16x16">
+    <link href="contact.css" rel="stylesheet">
+    <link href="hover.css" rel="stylesheet">
     <meta charset="UTF-8">
     <title>Multiservice Voorburg</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
           integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous"/>
     <link href="https://fonts.googleapis.com/css?family=Assistant&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
 <main>
@@ -35,11 +30,11 @@
 
         </ul>
     </nav>
-
     <section>
         <h1 class="contact-title">Stuur ons een berichtje</h1>
         <div class="container">
-            <form action="contactform.php" class="contact-form" id="form" method="post">
+            <form action="contactform.php"
+                  class="contact-form" id="form" method="post">
                 <label for="fname">Volledige Naam</label>
                 <input type="text" id="fname" name="name" placeholder="Uw naam" required>
 
@@ -50,10 +45,8 @@
                 <input type="text" id="subject" name="subject" placeholder="Uw onderwerp" required>
 
                 <label for="message">Uw bericht</label>
-                <textarea id="message" name="message" placeholder="Typ hier uw bericht" style="height:200px"
-                          required></textarea>
-                <div class="g-recaptcha" data-sitekey="6LfFjPwUAAAAAIutISYX4UB7vsopwIhqUo7pHFRt"
-                     data-callback="callback"></div>
+                <textarea id="message" name="message" placeholder="Typ hier uw bericht" style="height:200px"></textarea>
+                <div class="g-recaptcha" data-sitekey="6LeJn_wUAAAAAJsGJY_rkggeym_nBK79QtiLw23l"></div>
                 <input type="submit" value="Submit" name="submit">
             </form>
         </div>
--- contactform.php
+++ contactform.php
@@ -1,15 +1,68 @@
 <?php
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
-    $message = $_POST['message'];
+//if (isset($_POST['submit'])) {
+//    $name = $_POST['name'];
+//    $subject = $_POST['subject'];
+//    $mailFrom = $_POST['mail'];
+//    $message = $_POST['message'];
+//
+//    $mailTo = "grimrain@yahoo.com";
+//    $headers = "Van: " . $mailFrom;
+//    $txt = "Je hebt een email ontvangen van " . $name . ".\n\n" . $message;
+//
+//
+//    mail($mailTo, $subject, $txt, $headers);
+//    header("Location: index.html?mailsend");
+//}
 
-    $mailTo = "grimrain@yahoo.com";
-    $headers = "Van: " . $mailFrom;
-    $txt = "Je hebt een email ontvangen van " . $name . ".\n\n" . $message;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    function post_captcha($user_response)
+    {
+        $fields_string = '';
+        $fields = array(
+            'secret' => '6LeJn_wUAAAAAMLkaDHHZVTQKi4Ow7Q9Z2c7LAjC',
+            'response' => $user_response
+        );
+        foreach ($fields as $key => $value)
+            $fields_string .= $key . '=' . $value . '&';
+        $fields_string = rtrim($fields_string, '&');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($result, true);
+    }
+
+    // Call the function post_captcha
+    $res = post_captcha($_POST['g-recaptcha-response']);
+
+    if (!$res['success']) {
+        // What happens when the CAPTCHA wasn't checked
+        header("Location: FormFailed.html");
+    } else {
+        // If CAPTCHA is successfully completed...
 
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.html?mailsend");
+        if (isset($_POST['submit'])) {
+            $name = $_POST['name'];
+            $subject = $_POST['subject'];
+            $mailFrom = $_POST['mail'];
+            $message = $_POST['message'];
+
+            $mailTo = "grimrain@yahoo.com";
+            $headers = "Van: " . $mailFrom;
+            $txt = "Je hebt een email ontvangen van " . $name . ".\n\n" . $message;
+
+
+            mail($mailTo, $subject, $txt, $headers);
+            header("Location: FormSucceed.html");
+        }
+        echo '<br><p>CAPTCHA was completed successfully!</p><br>';
+    }
+} else {
 }
